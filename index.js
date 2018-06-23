50497471e51d4c3b3c9d3a31adcccc22ec136df9
Savannah Pyle <bea12022@byui.edu>
2018-06-23 14:24:13 -0600

    add cool

--- index.js
+++ index.js
@@ -1,12 +1,52 @@
-const cool = require('cool-ascii-faces')
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+var express = require('express');
+var app = express();
+var url = require('url');
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/math', function(request, response) {
+	handleMath(request, response);
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+function handleMath(request, response) {
+	var requestUrl = url.parse(request.url, true);
+
+	console.log("Query parameters: " + JSON.stringify(requestUrl.query));
+
+	var mail = requestUrl.query.mail;
+	var weight = Number(requestUrl.query.weight);
+
+	computeOperation(response, mail, weight);
+}
+
+function computeOperation(response, m, w) {
+	m = m.toLowerCase();
+
+	var result = 0;
+
+	if(m == "lstamped") {
+		result = w * .50;
+	} else if (m == "lmetered") {
+		result = w * .47;
+	} else if (m == "large") {
+		result = w * 1;
+	} else if (m == "first") {
+		result = w * 3.50;
+	} else {
+
+	}
+
+	var params = {mail: m, weight: w, result: result};
+	response.render('pages/result', params);
+
+}in
